9d4a88e60dd4e58ddc8b1e674549d52386e5f827
HarryPage <66968844+hkharekrushna@users.noreply.github.com>
2021-08-13 01:56:13 +0530

    final

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-
-
-function compute()
-{
-    var p = document.getElementById("principal").innerText.value;
-    p=parseFloat(p);
-    
-    var r = document.getElementById("rate").innerText.value;
-    var t = document.getElementById("years").innerText.value;
-    r=parseFloat(r);
-    t=parseFloat(t);
-
-    var si=(p*t*r)/100;
-    var date= new Date();
-    var y=date.getFullYear()+t;
-    console.log("kKKKKKKKKKKKKKKKKKK");
-
-    console.log(p+" "+r+" "+t+" "+si+" "+y);
-    var result = document.getElementById("result");
-    result.innerHTML="<p>If you deposit "+p+"<br> at an interest rate of "+ 
-    r+"% <br> You will receive an amount of "+p+si+"<br> in the year "+y+" </p>";
-
-    
+function showlatest() {
+    var rt = document.getElementById("rate").value;
+    document.getElementById("rv").innerText = rt + '%';
+    document.getElementById("errorP").style.display = "none";
+    document.getElementById("errorT").style.display = "none";
 }
 
-console.log("kKKKKKKKKKKKKKKKKKKMMMMMMMMMMMMMMMMMMM");
-        
+function showRV() {
+    var rt = document.getElementById("rate").value;
+    document.getElementById("rv").innerText = rt + '%'
+}
+
+function compute() {
+    var p = document.getElementById("principal").value;
+    console.log(p);
+
+
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    p = parseFloat(p);
+    r = parseFloat(r);
+    t = parseFloat(t);
+
+    if (isNaN(p)) {
+        document.getElementById("errorP").style.display = "block";
+        document.getElementById("errorT").style.display = "none";
+    } else if (isNaN(t)) {
+        document.getElementById("errorP").style.display = "none";
+        document.getElementById("errorT").style.display = "block";
+    } else {
+        document.getElementById("errorP").style.display = "none";
+        document.getElementById("errorT").style.display = "none";
+        var si = (p * t * r) / 100;
+        var total = p + si;
+        var date = new Date();
+        var y = date.getFullYear() + t;
+
+        console.log(p + " " + r + " " + t + " " + si + " " + y);
+        var result = document.getElementById("result");
+        result.innerHTML = "<p>If you deposit " + p + "<br> at an interest rate of " +
+            r + "% <br> You will receive an amount of " + total + "<br> in the year " + y + " </p>";
+
+    }
+
+
+
+
+
+
+
+    console.log("kKKKKKKKKKKKKKKKKKKMMMMMMMMMMMMMMMMMMM");
+
+
+}
+
+
