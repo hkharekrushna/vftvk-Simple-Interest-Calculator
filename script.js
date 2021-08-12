ca91743f117985452a4ce66cddce8e1d552591e1
HarryPage <66968844+hkharekrushna@users.noreply.github.com>
2021-08-12 16:41:09 +0530

    hkm new

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").innerText.value;
+    p=parseFloat(p);
+    
+    var r = document.getElementById("rate").innerText.value;
+    var t = document.getElementById("years").innerText.value;
+    r=parseFloat(r);
+    t=parseFloat(t);
+
+    var si=(p*t*r)/100;
+    var date= new Date();
+    var y=date.getFullYear()+t;
+    console.log("kKKKKKKKKKKKKKKKKKK");
+
+    console.log(p+" "+r+" "+t+" "+si+" "+y);
+    var result = document.getElementById("result");
+    result.innerHTML="<p>If you deposit "+p+"<br> at an interest rate of "+ 
+    r+"% <br> You will receive an amount of "+p+si+"<br> in the year "+y+" </p>";
+
     
 }
+
+console.log("kKKKKKKKKKKKKKKKKKKMMMMMMMMMMMMMMMMMMM");
         
